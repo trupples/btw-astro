85acce9e57df07835d78984a8c2c99acd84f7c53
Ioan Dragomir <123ioandragomir123@gmail.com>
2022-11-26 18:20:47 +0200

    Set all paths to be absolute
        This should really be fixed another way

--- src/components/BTWFirstSection.tsx
+++ src/components/BTWFirstSection.tsx
@@ -2,7 +2,7 @@ import './BTWFirstSection.css';
 
 export default function BTWFirstSection() {
 	return <section id="first-section">
-		<img id="logo" src="logo.png" alt="BEST Training Weekend logo" width="300" />
+		<img id="logo" src="/logo.png" alt="BEST Training Weekend logo" width="300" />
 		<h1>BEST Training Weekend</h1>
 		<h2>Dezvoltă-ți abilitățile!</h2>
 		<br/><br/>
@@ -10,8 +10,8 @@ export default function BTWFirstSection() {
 		<h2><a href="https://goo.gl/maps/MKMRF2eEGbCtvk8T7" target="_blank">Tech'n Trade</a></h2>
 		<br/><br/>
 		<h2><a href="https://docs.google.com/forms/d/e/1FAIpQLSf74FuFSKZzq1OsnyNJ0J542nArlt4xdlRq-5fzDw7fPTI2NQ/viewform">Înscrie-te!</a></h2>
-		<img id="leaves-left" src="page1-left-leaves.png" />
-		<img id="leaves-right" src="page1-right-leaves.png" />
+		<img id="leaves-left" src="/page1-left-leaves.png" />
+		<img id="leaves-right" src="/page1-right-leaves.png" />
 		<div id="color-change"></div>
 	</section>;
 }
--- src/components/ContactCard.tsx
+++ src/components/ContactCard.tsx
@@ -9,7 +9,7 @@ export interface Props {
 
 export default function ContactCard({ name, role, email, tel } : Props) {
 	return <div class='ContactCard'>
-		<img src={'people/' + name + '.png'} width='200' />
+		<img src={'/people/' + name + '.png'} width='200' />
 		<span class='name'>{name}</span>
 		<span class='role'>{role}</span>
 		<span class='email'><a href={'mailto:' + email}>{email}</a></span>
