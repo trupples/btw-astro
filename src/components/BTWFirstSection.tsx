af8ec347174e3a754d31c7d3f1286a8c0467f9bb
Ioan Dragomir <123ioandragomir123@gmail.com>
2022-11-26 17:18:50 +0200

    Fix typo in location name and rasterize logo

--- src/components/BTWFirstSection.tsx
+++ src/components/BTWFirstSection.tsx
@@ -2,12 +2,12 @@ import './BTWFirstSection.css';
 
 export default function BTWFirstSection() {
 	return <section id="first-section">
-		<img id="logo" src="logo.svg" alt="BEST Training Weekend logo" width="300" />
+		<img id="logo" src="logo.png" alt="BEST Training Weekend logo" width="300" />
 		<h1>BEST Training Weekend</h1>
 		<h2>Dezvoltă-ți abilitățile!</h2>
 		<br/><br/>
 		<h2>10 - 11 Decembrie</h2>
-		<h2><a href="https://goo.gl/maps/MKMRF2eEGbCtvk8T7" target="_blank">Tech'n'Trade</a></h2>
+		<h2><a href="https://goo.gl/maps/MKMRF2eEGbCtvk8T7" target="_blank">Tech'n Trade</a></h2>
 		<br/><br/>
 		<h2><a href="https://docs.google.com/forms/d/e/1FAIpQLSf74FuFSKZzq1OsnyNJ0J542nArlt4xdlRq-5fzDw7fPTI2NQ/viewform">Înscrie-te!</a></h2>
 		<img id="leaves-left" src="page1-left-leaves.png" />
