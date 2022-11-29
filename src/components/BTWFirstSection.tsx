ec2130321a85f2a23af4bc6e93b2d2477188b799
Ioan Dragomir <123ioandragomir123@gmail.com>
2022-11-29 17:12:44 +0200

    Changed location of BEST and UTCN logos, removed bad event logo

--- src/components/BTWFirstSection.tsx
+++ src/components/BTWFirstSection.tsx
@@ -2,7 +2,10 @@ import './BTWFirstSection.css';
 
 export default function BTWFirstSection() {
 	return <section id="first-section">
-		<img id="logo" src="/logo.png" alt="BEST Training Weekend logo" width="300" />
+		<div id="logos">
+			<a href='https://bestcj.ro'><img src="/BESTCJ_signature.png" alt="BEST Cluj-Napoca logo" height="100" /></a>
+			<a href='https://utcluj.ro'><img src="/UT_Logo.png" alt="Technical University of Cluj-Napoca logo" height="100" /></a>
+		</div>
 		<h1>BEST Training Weekend</h1>
 		<h2>Dezvoltă-ți abilitățile!</h2>
 		<br/><br/>
@@ -10,10 +13,6 @@ export default function BTWFirstSection() {
 		<h2><a href="https://goo.gl/maps/MKMRF2eEGbCtvk8T7" target="_blank">Tech'n Trade</a></h2>
 		<br/><br/>
 		<h2><a href="https://docs.google.com/forms/d/e/1FAIpQLSf74FuFSKZzq1OsnyNJ0J542nArlt4xdlRq-5fzDw7fPTI2NQ/viewform">Înscrie-te!</a></h2>
-		<div id="logos">
-			<a href='https://bestcj.ro'><img src="/BESTCJ_signature.png" alt="BEST Cluj-Napoca logo" height="100" /></a>
-			<a href='https://utcluj.ro'><img src="/UT_Logo.png" alt="Technical University of Cluj-Napoca logo" height="100" /></a>
-		</div>
 		<img id="leaves-left" src="/page1-left-leaves.png" />
 		<img id="leaves-right" src="/page1-right-leaves.png" />
 		<div id="color-change"></div>
